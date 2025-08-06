efd1dca2c41da4c29d42602b94a37e15c3cb3415
ilyassatlassi <ilyassatlassi@gmail.com>
2025-08-06 01:16:38 +0100

    add some change

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ async fn main() {
         clear_background(BLACK);
         app.draw();
         app.check_update();
+        draw_text(
+            "Press ENTER to start smoothing, SPACE || ESC to Exit.",
+            20.0,
+            screen_height() - 20.0,
+            20.0,
+            GRAY,
+        );
         // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
